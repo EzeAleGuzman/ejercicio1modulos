29a592280a7268d5ac3b1f486c3beef467840df4
sarabe89 <ezealeguzman@gmail.com>
2023-04-27 22:15:15 -0300

    se agrega ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+import operaciones_basicas as Op
+
+sum = Op.suma(10, 11)
+print(sum)
+res = Op.resta(355,243)
+print(res)
+mul = Op.multiplicar(15,22)
+print(mul)
+div = Op.division(300,30)
+print(div)
